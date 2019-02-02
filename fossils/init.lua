2c8bf04b230cbc465d85db8a922921f9e6790b72
MetricTycho <46092636+MetricTycho@users.noreply.github.com>
2019-02-02 11:42:19 +0100

    Add files via upload

--- fossils/init.lua
+++ fossils/init.lua
@@ -1,3 +1,5 @@
+--nodes
+
 minetest.register_node("fossils:ammonite", {
 	description = "Ammonite",
 	tiles = {"fossils_ammonite.png"},
@@ -6,7 +8,6 @@ minetest.register_node("fossils:ammonite", {
 	drop = {
 		max_items = 1,
 		items = {
-			{items = {'default:iron_lump 9'}, rarity = 8},
 			{items = {'fossils:ammonite'}, rarity = 4},
 			{items = {'default:sandstone'}}
 		}
@@ -22,7 +23,6 @@ minetest.register_node("fossils:trilobite", {
 	drop = {
 		max_items = 1,
 		items = {
-			{items = {'default:iron_lump 9'}, rarity = 8},
 			{items = {'fossils:trilobite'}, rarity = 4},
 			{items = {'default:sandstone'}}
 		}
@@ -37,7 +37,7 @@ minetest.register_node("fossils:fern", {
 	drop = {
 		max_items = 1,
 		items = {
-			{items = {'default:coal_lump 9'}, rarity = 5},
+			{items = {'default:coal_lump'}, rarity = 5},
 			{items = {'fossils:fern'}, rarity = 2},
 			{items = {'default:sandstone'}}
 		}
@@ -52,7 +52,6 @@ minetest.register_node("fossils:seymouria", {
 	drop = {
 		max_items = 1,
 		items = {
-			{items = {'default:coal_lump 9'}, rarity = 5},
 			{items = {'fossils:fern'}, rarity = 2},
 			{items = {'default:sandstone'}}
 		}
@@ -61,54 +60,118 @@ minetest.register_node("fossils:seymouria", {
 })
 
 minetest.register_node("fossils:amber", {
-	description = "Ambber",
+	description = "Amber",
+	drawtype="glasslike",
+	paramtype="light",
+	use_texture_alpha = true,
+	sunlight_propagates=true,
 	tiles = {"fossils_amber.png"},
 	groups = {cracky=1, stone=1},
-	drop = {
-		max_items = 1,
-		items = {
-			{items = {'default:coal_lump 9'}, rarity = 5},
-			{items = {'fossils:fern'}, rarity = 2},
-			{items = {'default:sandstone'}}
-		}
-	}
-
+	drop = 'fossils:amber',
+	sounds = default.node_sound_stone_defaults(),
 })
+
+
+
+--Ores
 
 
 minetest.register_ore({
 	ore_type = "scatter",
 	ore = "fossils:ammonite",
 	wherein = "default:sandstone",
-	clust_scarcity = 6*6*6,
-	clust_num_ores = 8,
-	clust_size = 3,
+	clust_scarcity = 7*7*7,
+	clust_num_ores = 1,
+	clust_size = 1,
 	y_min = -31000,
 	y_max = 5,
 })
 
 minetest.register_ore({
 	ore_type = "scatter",
-	ore = "fossils:ammonite",
-	wherein = "default:stone",
-	clust_scarcity = 6*6*6,
-	clust_num_ores = 8,
-	clust_size = 3,
-	y_min = -31000,
-	y_max = 5,
-})
-
-
-minetest.register_ore({
-	ore_type = "scatter",
-	ore = "default:sandstone_brick",
-	wherein = "default:stone",
+	ore = "fossils:trilobite",
+	wherein = "default:sandstone",
 	clust_scarcity = 8*8*8,
-	clust_num_ores = 6,
-	clust_size = 5,
+	clust_num_ores = 1,
+	clust_size = 1,
 	y_min = -31000,
-	y_max = -5,
+	y_max = 5,
 })
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "fossils:fern",
+	wherein = "default:sandstone",
+	clust_scarcity = 6*6*6,
+	clust_num_ores = 1,
+	clust_size = 1,
+	y_min = -31000,
+	y_max = 5,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "fossils:seymouria",
+	wherein = "default:sandstone",
+	clust_scarcity = 10*10*10,
+	clust_num_ores = 1,
+	clust_size = 1,
+	y_min = -31000,
+	y_max = 5,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "fossils:amber",
+	wherein = "default:gravel",
+	clust_scarcity = 12*12*12,
+	clust_num_ores = 1,
+	clust_size = 1,
+	y_min = -31000,
+	y_max = -50,
+})
+
+--selling fossils
+
+minetest.register_craft({
+	output = "default:diamond 5",
+	recipe = {
+		{'fossils:ammonite', 'fossils:ammonite', 'fossils:ammonite'},
+		{'fossils:ammonite', 'fossils:ammonite', 'fossils:ammonite'},
+		{'fossils:ammonite', 'fossils:ammonite', 'fossils:ammonite'}
+		}
+})
+
+minetest.register_craft({
+	output = "default:diamond 7",
+	recipe = {
+		{'fossils:trilobite', 'fossils:trilobite', 'fossils:trilobite'},
+		{'fossils:trilobite', 'fossils:trilobite', 'fossils:trilobite'},
+		{'fossils:trilobite', 'fossils:trilobite', 'fossils:trilobite'}
+		}
+})
+
+minetest.register_craft({
+	output = "default:diamond 10",
+	recipe = {
+		{'fossils:seymouria', 'fossils:seymouria', 'fossils:seymouria'},
+		{'fossils:seymouria', 'fossils:seymouria', 'fossils:seymouria'},
+		{'fossils:seymouria', 'fossils:seymouria', 'fossils:seymouria'}
+		}
+})
+	
+minetest.register_craft({
+	output = "default:diamond 3",
+	recipe = {
+		{'fossils:fern', 'fossils:fern', 'fossils:fern'},
+		{'fossils:fern', 'fossils:fern', 'fossils:fern'},
+		{'fossils:fern', 'fossils:fern', 'fossils:fern'}
+		}
+})
+	
+	
+
+
 
 
 
